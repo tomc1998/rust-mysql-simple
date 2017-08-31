5a3c94a6d99bc4b3e1aef73c4ce4c5964b0394ac
Tom Cheng <thomascheng1998@googlemail.com>
2017-08-31 16:48:31 +0100

    Finished connection mocking implementations.
    Re-exported all implementations from mock_impl in conn.
    
    MockStmt and MockQueryResult are not yet correctly documented or tested
    as a valid mock implementation, nor do they have appropriate helper
    methods / defaults.

--- src/conn/mock_impl.rs
+++ src/conn/mock_impl.rs
@@ -7,15 +7,19 @@
 //! the functions and recording the data to compare in the future for testing the behaviour without
 //! an actual DB connection.
 
-use error;
+use std::hash::BuildHasherDefault as BldHshrDflt;
+use std::collections::HashMap;
+use fnv::FnvHasher;
 use {Params, Value, Column, FromValueError, from_value, from_value_opt};
-use super::GenericConnection;
+use super::{GenericConnection, GenericRow, GenericQueryResult, GenericStmt};
+use error::Result as MyResult;
 use prelude::*;
 use std::sync::Arc;
 
 /// A struct representing a type of request for a value from a row - i.e. get or take. These are
 /// stored inside a MockRow, for developers to query and write tests based on the value.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[allow(dead_code)]
 pub enum RowRequestType {
     Get,
     Take,
@@ -25,16 +29,16 @@ pub enum RowRequestType {
 ///
 /// The dev provides dummy values to return when various row methods are called, and can query the
 /// mock row to see which values and columns have been requested and which haven't.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct MockRow {
     /// The list of values in this mock row.
-    values: Vec<Option<Value>>,
+    pub values: Vec<Option<Value>>,
     /// The list of columns in this mock row. These indices will match the values in the values
     /// vec.
-    columns: Arc<Vec<Column>>,
+    pub columns: Arc<Vec<Column>>,
 
     /// A list, in order, of the requests for columns made to this mock row.
-    requests: Vec<(Column, RowRequestType)>,
+    pub requests: Vec<(Column, RowRequestType)>,
 }
 
 impl GenericRow for MockRow {
@@ -104,9 +108,69 @@ impl GenericRow for MockRow {
     }
 }
 
+pub struct MockStmt {
+    pub params: Option<Vec<Column>>,
+    pub columns: Option<Vec<Column>>,
+    pub column_indexes: HashMap<String, usize, BldHshrDflt<FnvHasher>>,
+    pub fn_execute: Option<Box<Fn(&str, Params) -> MyResult<MockQueryResult>>>,
+    pub fn_first_exec: Option<Box<Fn(&str, Params) -> MyResult<Option<MockRow>>>>,
+    pub query: String,
+}
 
-pub struct MockStmt;
-pub struct MockQueryResult;
+impl<'a> GenericStmt<'a> for MockStmt {
+    type QueryResult = MockQueryResult;
+    type Row = MockRow;
+
+    fn params_ref(&self) -> Option<&[Column]> { self.params.as_ref().map(|v| &v[..]) }
+    fn columns_ref(&self) -> Option<&[Column]> { self.columns.as_ref().map(|v| &v[..]) }
+    fn column_index<T: AsRef<str>>(&self, name: T) -> Option<usize> {
+        self.column_indexes.get(&name.as_ref().to_owned()).cloned()
+    }
+    fn execute<T: Into<Params>>(&'a mut self, params: T) -> MyResult<Self::QueryResult> {
+        if self.fn_execute.is_some() { self.fn_execute.as_mut().unwrap()(&self.query, params.into()) }
+        else { 
+            panic!("Tried to call execute() on a mock statement without and implementation") 
+        }
+    }
+    fn first_exec<T: Into<Params>>(&'a mut self, params: T) -> MyResult<Option<Self::Row>> {
+        if self.fn_first_exec.is_some() { self.fn_first_exec.as_mut().unwrap()(&self.query, params.into()) }
+        else { 
+            panic!("Tried to call execute() on a mock statement without and implementation") 
+        }
+    }
+}
+
+pub struct MockQueryResult {
+    pub affected_rows: u64,
+    pub last_insert_id: u64,
+    pub warnings: u16,
+    pub info: Vec<u8>,
+    pub column_indexes: HashMap<String, usize, BldHshrDflt<FnvHasher>>,
+    pub columns: Vec<Column>,
+    pub more_results_exists: bool,
+    pub rows: Option<Vec<MyResult<MockRow>>>,
+}
+
+impl Iterator for MockQueryResult {
+    type Item = MyResult<MockRow>;
+    fn next(&mut self) -> Option<MyResult<MockRow>> { 
+        if self.rows.is_none() { return None }
+        self.rows.as_mut().unwrap().pop()
+    } 
+}
+
+impl GenericQueryResult  for MockQueryResult {
+    fn affected_rows(&self) -> u64 { self.affected_rows }
+    fn last_insert_id(&self) -> u64 { self.last_insert_id }
+    fn warnings(&self) -> u16 { self.warnings }
+    fn info(&self) -> Vec<u8> { self.info.clone() }
+    fn column_index<T: AsRef<str>>(&self, name: T) -> Option<usize> {
+        self.column_indexes.get(&name.as_ref().to_owned()).cloned()
+    }
+    fn column_indexes(&self) -> HashMap<String, usize, BldHshrDflt<FnvHasher>> { self.column_indexes.clone() }
+    fn columns_ref(&self) -> &[Column] { &self.columns[..] }
+    fn more_results_exists(&self) -> bool { self.more_results_exists }
+}
 
 /// Mock implementation for a DB connection.
 ///
@@ -126,12 +190,13 @@ pub struct MockQueryResult;
 ///
 ///     });
 /// ```
+#[allow(dead_code)]
 pub struct MockConnection {
-    fn_query: Option<Box<Fn(&str) -> error::Result<MockQueryResult>>>,
-    fn_first: Option<Box<Fn(&str) -> error::Result<Option<MockRow>>>>,
-    fn_prepare: Option<Box<Fn(&str) -> error::Result<MockStmt>>>,
-    fn_prep_exec: Option<Box<Fn(&str, Params) -> error::Result<MockQueryResult>>>,
-    fn_first_exec: Option<Box<Fn(&str, Params) -> error::Result<Option<MockRow>>>>,
+    pub fn_query: Option<Box<Fn(&str) -> MyResult<MockQueryResult>>>,
+    pub fn_first: Option<Box<Fn(&str) -> MyResult<Option<MockRow>>>>,
+    pub fn_prepare: Option<Box<Fn(&str) -> MyResult<MockStmt>>>,
+    pub fn_prep_exec: Option<Box<Fn(&str, Params) -> MyResult<MockQueryResult>>>,
+    pub fn_first_exec: Option<Box<Fn(&str, Params) -> MyResult<Option<MockRow>>>>,
 }
 
 impl<'a> GenericConnection<'a> for MockConnection {
@@ -139,7 +204,7 @@ impl<'a> GenericConnection<'a> for MockConnection {
     type Stmt = MockStmt;
     type Row = MockRow;
 
-    fn query<Q: AsRef<str>>(&mut self, query: Q) -> error::Result<Self::QueryResult> {
+    fn query<Q: AsRef<str>>(&mut self, query: Q) -> MyResult<Self::QueryResult> {
         if self.fn_query.is_some() {
             self.fn_query.as_ref().unwrap()(query.as_ref())
         } else {
@@ -147,7 +212,7 @@ impl<'a> GenericConnection<'a> for MockConnection {
         }
     }
 
-    fn first<Q: AsRef<str>>(&mut self, query: Q) -> error::Result<Option<Self::Row>> {
+    fn first<Q: AsRef<str>>(&mut self, query: Q) -> MyResult<Option<Self::Row>> {
         if self.fn_first.is_some() {
             self.fn_first.as_ref().unwrap()(query.as_ref())
         } else {
@@ -155,7 +220,7 @@ impl<'a> GenericConnection<'a> for MockConnection {
         }
     }
 
-    fn prepare<Q: AsRef<str>>(&mut self, query: Q) -> error::Result<Self::Stmt> {
+    fn prepare<Q: AsRef<str>>(&mut self, query: Q) -> MyResult<Self::Stmt> {
         if self.fn_prepare.is_some() {
             self.fn_prepare.as_ref().unwrap()(query.as_ref())
         } else {
@@ -163,7 +228,7 @@ impl<'a> GenericConnection<'a> for MockConnection {
         }
     }
 
-    fn prep_exec<Q, P>(&mut self, query: Q, params: P) -> error::Result<Self::QueryResult>
+    fn prep_exec<Q, P>(&mut self, query: Q, params: P) -> MyResult<Self::QueryResult>
     where
         Q: AsRef<str>,
         P: Into<Params>,
@@ -175,7 +240,7 @@ impl<'a> GenericConnection<'a> for MockConnection {
         }
     }
 
-    fn first_exec<Q, P>(&mut self, query: Q, params: P) -> error::Result<Option<Self::Row>>
+    fn first_exec<Q, P>(&mut self, query: Q, params: P) -> MyResult<Option<Self::Row>>
     where
         Q: AsRef<str>,
         P: Into<Params>,
